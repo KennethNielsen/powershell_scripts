4991fb6f2ca45596420c137bfcd2aeefd015da07
Kenneth Nielsen <k.nielsen81@gmail.com>
2021-01-11 09:16:30 +0100

    Fixed tab completion and added grep alias

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,14 @@
+# Fix powershells broken tab behavior
+Set-PSReadlineKeyHandler -Key Tab -Function Complete
+
+# Unix style aliases
 New-Alias which get-command
+new-alias grep findstr
+
+# Command aliases
 New-Alias git "C:\Program Files\Git\bin\git.exe"
 New-Alias emacs "C:\Program Files\emacs-25.3_1-x86_64\bin\runemacs.exe"
 
+# Make this folder part of part, so that script files in it can be executed as commands
 $ProfileRoot = (Split-Path -Parent $MyInvocation.MyCommand.Path)
 $env:path += ";$ProfileRoot"
