fb12c729bfca79af90bfa3dc61d906453e683b50
Kenneth Nielsen <kni@spectroinlets.com>
2021-04-14 11:49:02 +0200

    Remove git alias, since newer git installers offers to manipulate PATH

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -6,8 +6,9 @@ New-Alias which get-command
 new-alias grep findstr
 
 # Command aliases
-New-Alias git "C:\Program Files\Git\bin\git.exe"
 New-Alias emacs "C:\Program Files\emacs-25.3_1-x86_64\bin\runemacs.exe"
+# NOTE git alias no longer necessary, reinstall git and let the installer modify PATH
+#New-Alias git "C:\Program Files\Git\bin\git.exe"
 
 # Make this folder part of part, so that script files in it can be executed as commands
 $ProfileRoot = (Split-Path -Parent $MyInvocation.MyCommand.Path)
